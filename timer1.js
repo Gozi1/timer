1b2186c193a79e263912603350094569b505f1f3
Gozi1 <snwabiani@gmail.com>
2022-10-14 13:20:37 +0000

    add timer1

--- timer1.js
+++ timer1.js
@@ -1,10 +1,12 @@
-
+// reads input from console
 const inputAlarms = process.argv.slice(2);
 
 const timer = function(alarms){
+  // No alarms just end program
   if(alarms.length === 0) {
     return;
   }
+  // iterates through the alarms and multiples the parseTime by 1000  to get secs
   for(time of alarms){
     let parsedTime = parseInt(time)
     if(time < 0 || !Number.isInteger(parsedTime)){
@@ -18,4 +20,5 @@ const timer = function(alarms){
   }
 }
 
+// runs function
 timer(inputAlarms);
